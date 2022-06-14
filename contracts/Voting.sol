82282f9b5afafa5a7cd46fbf95f14e36b546debd
mikesolg <76155783+mikesolg@users.noreply.github.com>
2022-06-14 14:13:26 +0200

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -25,16 +25,18 @@ enum WorkflowStatus {
 
 contract Voting is Ownable {
     // allow multiple proposal / voting sessions 
+    // when sessionId changes, a new series of proposals and votes is available
     uint public currentSessionId;
 
-    // potential voters 
-    // mapping(address=>Voter) public voters;
+    // potential voters - please note that registration is stored in sessionId 0
+    // sessionId => address => voter 
     mapping(uint=>mapping(address=>Voter)) public voters;
     
-    // session id => proposal Id => proposal
+    // sessionId => proposalId => proposal
+    // proposal Id 0 is an empty proposal which is the winner in case of equality between votes
     mapping(uint => mapping(uint=>Proposal)) public proposals;
 
-    // next proposal index to be created inside this session
+    // Id of the last created proposal inside this session 
     uint public lastProposalId;
     uint public winningProposalId;
 
@@ -47,64 +49,54 @@ contract Voting is Ownable {
     event Voted (address voter, uint proposalId);
 
     modifier onlyRegistered(address _addr) {
-        // registering is stored in first session and may be modified later by admin 
+        // registering is stored in first voting session only 
         require(voters[0][msg.sender].isRegistered, "Only registered users can interact with this contract.");
         _;
     }
 
+   function registerUser(address _addr) public onlyOwner {
+        require(currentStatus==WorkflowStatus.RegisteringVoters, "Registrations not opened.");
+        // registration is stored in first session but new addresses may be added in later sessions 
+        voters[0][_addr].isRegistered=true;
+        emit VoterRegistered(_addr);
+    }
+
     function startRegisteringProposals() public onlyOwner {
-        require (currentStatus==WorkflowStatus.RegisteringVoters);
+        require (currentStatus==WorkflowStatus.RegisteringVoters, "Expected current status is Registering Users");
         currentStatus=WorkflowStatus.ProposalsRegistrationStarted;
         emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationStarted);
     }
 
     function stopRegisteringProposals() public onlyOwner {
-        require (currentStatus==WorkflowStatus.ProposalsRegistrationStarted);
+        require (currentStatus==WorkflowStatus.ProposalsRegistrationStarted, "Expected current status is Proposals Registrations Started");
         currentStatus=WorkflowStatus.ProposalsRegistrationEnded;
-        emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationEnded);
+        emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, WorkflowStatus.ProposalsRegistrationEnded);
     }
 
     function startVoting() public onlyOwner {
-       // require (currentStatus==WorkflowStatus.ProposalsRegistrationEnded);
+        require (currentStatus==WorkflowStatus.ProposalsRegistrationEnded, "Expected current status is Proposals Registrations Ended");
         currentStatus=WorkflowStatus.VotingSessionStarted;
         emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationEnded, WorkflowStatus.VotingSessionStarted);
     }
 
     function stopVoting() public onlyOwner {
-        //require (currentStatus==WorkflowStatus.VotingSessionStarted);
+        require (currentStatus==WorkflowStatus.VotingSessionStarted, "Expected current status is Voting Session Started");
         currentStatus=WorkflowStatus.VotingSessionEnded;
-        
-        // max vote number found for a proposal
-        uint max;
-
-        // memory variable to prevent multiple storage updates
-        uint winningIndex;
-
-        for (uint i=1;i<=lastProposalId;i++) {
-            if (proposals[currentSessionId][i].voteCount==max) {
-                // equality => no winner, keep max the same
-                winningIndex=0; 
-            } else if (proposals[currentSessionId][i].voteCount>max) {
-                winningIndex=i;
-                max=proposals[currentSessionId][i].voteCount;
-            }
-        }
-        
-        // update public member
-        winningProposalId=winningIndex;
         emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
     }
 
     function registerProposal(string calldata _descr) public onlyRegistered(msg.sender) {
-        require(currentStatus==WorkflowStatus.ProposalsRegistrationStarted, "Proposal registrations not opened.");
+        require (currentStatus==WorkflowStatus.ProposalsRegistrationStarted, "Proposal registrations not opened");
         // create a new proposal inside current session
         proposals[currentSessionId][++lastProposalId]=Proposal(_descr, 0);
         emit ProposalRegistered(lastProposalId);
     }
 
+    // a user voting for propId 0 is considered a blank vote with the 
+    // only effect that he cannot vote inside this voting session anymore
     function vote(uint _propId) public onlyRegistered(msg.sender)  {
-        //require (currentStatus==WorkflowStatus.VotingSessionStarted);
-        //require (!voters[currentSessionId][msg.sender].hasVoted, "You have already voted !");
+        require (currentStatus==WorkflowStatus.VotingSessionStarted, "Voting session not opened");
+        require (!voters[currentSessionId][msg.sender].hasVoted, "You have already voted !");
         require (_propId <= lastProposalId, "This proposal does not exist");
         voters[currentSessionId][msg.sender].hasVoted=true;
         voters[currentSessionId][msg.sender].votedProposalId=_propId;
@@ -112,15 +104,37 @@ contract Voting is Ownable {
         emit Voted(msg.sender, _propId);
     }
 
-    function registerUser(address _addr) public onlyOwner {
-        require(currentStatus==WorkflowStatus.RegisteringVoters, "Registrations not opened.");
-        voters[0][_addr].isRegistered=true;
-        emit VoterRegistered(_addr);
-    }
+    function publishResults() public onlyOwner { 
+        require (currentStatus==WorkflowStatus.VotingSessionEnded, "Expected current status is Voting Session Ended");
+        
+        // max vote number found for a proposal
+        uint max;
 
-    function reset() public onlyOwner {
+        // use this memory variable to prevent multiple storage updates
+        uint winningIndex;
+
+        // 0 doesnt need to be considered as it is used for equalities
+        for (uint i=1;i<=lastProposalId;i++) {
+            if (proposals[currentSessionId][i].voteCount==max) {
+                // equality => no winner
+                winningIndex=0; 
+            } else if (proposals[currentSessionId][i].voteCount>max) {
+                winningIndex=i;
+                max=proposals[currentSessionId][i].voteCount;
+            }
+        }
+        
+        // update public member - value stays 0 in case of equality
+        winningProposalId=winningIndex;
+
+        currentStatus=WorkflowStatus.VotesTallied;
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
+    }
+ 
+    // whatever the current status, the admin can start a new voting session
+    function createNewSession() public onlyOwner {
         currentStatus=WorkflowStatus.RegisteringVoters;
-        // this is the index of the new session
+        // index of the new session with no proposals and votes
         currentSessionId++;
         lastProposalId=0;
         winningProposalId=0;
